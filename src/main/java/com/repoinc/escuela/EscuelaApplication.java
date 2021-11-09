e90691eb8d077055b9d55f08d9cec1bbfd95bb5b
NelsonJaime <nlsnjaime@gmail.com>
2021-11-09 10:11:11 +0100

    Primeras configuraciones

---
+++ src/main/java/com/repoinc/escuela/EscuelaApplication.java
@@ -0,0 +1,13 @@
+package com.repoinc.escuela;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscuelaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EscuelaApplication.class, args);
+	}
+
+}
